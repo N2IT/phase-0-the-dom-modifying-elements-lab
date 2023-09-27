3251ce0c385f6d08294b024dbe8b727f62377f2d
Tony Eder <tonyeder11@gmail.com>
2023-09-27 14:21:31 -0700

    DON'T APPEND HAHAHAAAAAAA!

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const newElement = document.getElementById("main");
+newElement.remove();
+
+const newHeader = document.createElement("h1");
+  newHeader.id='victory';
+  newHeader.innerHTML="TONY-EDER is the champion";
